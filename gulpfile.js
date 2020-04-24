1e72a3d2185aba645bf74f0c11866d898ec42f4b
Ivan <dykyiivan@gmail.com>
2020-04-24 18:47:53 +0200

    fixed error in portfolio section

--- gulpfile.js
+++ gulpfile.js
@@ -76,7 +76,7 @@ gulp.task("js", function() {
       "node_modules/jquery/dist/jquery.js",
       "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
       "node_modules/slick-carousel/slick/slick.min.js",
-      "node_modules/mixitup/dist/mixitup.min.js"
+      "node_modules/mixitup/dist/mixitup.min.js",
     ])
     .pipe(plumber())
     .pipe(concat("all.min.js")) // якщо мініфакція одразу розширення min.js
